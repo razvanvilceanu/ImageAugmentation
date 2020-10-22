a6ed47a0bd7f13984825725362e1cddc32cebae7
razvan.vilceanu <razvanvilceanu@hotmail.com>
2020-10-22 17:07:21 +0300

    *initial commit

--- imgAug.py
+++ imgAug.py
@@ -1,12 +1,43 @@
 import imageio
 import imgaug as ia
 from imgaug import augmenters as iaa
-import numpy as np
-# ia.seed()
+import os, glob
 
-image = imageio.imread("https://upload.wikimedia.org/wikipedia/en/7/7d/Lenna_%28test_image%29.png")
-images = [image, image, image, image]
-rotate = iaa.Affine(rotate=(-25, 25))
+ia.seed(1)
 
-images_aug = rotate(images=images)
-ia.imshow(np.hstack(images_aug))
+
+def load_augment_save(path, ext):
+    augpath = path + "/augmented_images"
+    if not os.path.exists(augpath):
+        os.mkdir(augpath)
+    ext = "/*." + ext
+    print(path+ext)
+    images = glob.glob(path + ext)
+    print("Loaded images in %a" % path + "\n")
+    print(images)
+
+    for e in images:
+        img = imageio.imread(e)
+        print("Working on image %s" % e)
+        seq = iaa.Sequential([
+            iaa.Affine(rotate=(-25, 25)),
+            iaa.AdditiveGaussianNoise(scale=(10, 60)),
+            iaa.Crop(percent=(0, 0.2))
+        ])
+        img_aug = seq(image=img)
+        print("Augmented image %s" % e)
+        imageio.imsave(augpath, img_aug, format=".png")
+        print("Saved image %s on %a" % e % augpath)
+    return None
+
+
+load_augment_save("data/etrims-db_v1/annotations/04_etrims-ds", "png")
+
+# annots = glob.glob("data/etrims-db_v1/annotations/08_etrims-ds/.png")
+# for e in annots:
+#     if not(e.endswith(".jpg") or (e.endswith(".png"))):
+#         annots.remove(e)
+#
+#
+# for image in images:
+#     img = imageio.imread(image)
